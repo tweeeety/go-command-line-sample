ff4da660405282db49b61d8b0291b0cc94972796
tweeeety <tweeeety.0719@gmail.com>
2017-05-15 03:25:40 +0900

    cli_40

---
+++ src/script/cli_40/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "cli_04"
+	app.Usage = "cli_04 sample"
+
+	// command action
+	app.Commands = []cli.Command{
+		// `go run cli_04/main.go add パラメータ`でactionするコマンド
+		{
+			Name:    "add",
+			Aliases: []string{"a"},
+			Usage:   "add a task to the list",
+			Action: func(c *cli.Context) error {
+				fmt.Println("added task: ", c.Args().First())
+				return nil
+			},
+		},
+		// `go run cli_04/main.go complete パラメータ`でactionするコマンド
+		{
+			Name:    "complete",
+			Aliases: []string{"c"},
+			Usage:   "complete a task on the list",
+			Action: func(c *cli.Context) error {
+				fmt.Println("completed task: ", c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:    "template",
+			Aliases: []string{"t"},
+			Usage:   "options for task templates",
+			Subcommands: []cli.Command{
+				// `go run cli_04/main.go template add パラメータ`でactionするコマンド
+				{
+					Name:  "add",
+					Usage: "add a new template",
+					Action: func(c *cli.Context) error {
+						fmt.Println("new task template: ", c.Args().First())
+						return nil
+					},
+				},
+				// `go run cli_04/main.go template remove パラメータ`でactionするコマンド
+				{
+					Name:  "remove",
+					Usage: "remove an existing template",
+					Action: func(c *cli.Context) error {
+						fmt.Println("removed task template: ", c.Args().First())
+						return nil
+					},
+				},
+			},
+		},
+	}
+
+	app.Run(os.Args)
+}
